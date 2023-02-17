618d33642db5b09e593df730d755edc38b5275c3
gy6543721 <gy6543721@yahoo.com.tw>
2023-02-18 05:53:20 +0900

    BottomBar Refinement

--- app/src/main/java/levilin/moviedatabase/ui/MainActivity.kt
+++ app/src/main/java/levilin/moviedatabase/ui/MainActivity.kt
@@ -5,11 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Scaffold
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import levilin.moviedatabase.ui.navigation.BottomNavView
@@ -29,9 +35,24 @@ class MainActivity : ComponentActivity() {
         setContent {
             MovieDatabaseTheme {
                 val navController = rememberNavController()
-
-                Scaffold(bottomBar = { BottomNavView(navController = navController) }) { innerPadding ->
-                    NavGraphView(navController = navController, sharedViewModel = sharedViewModel, modifier = Modifier.padding(innerPadding))
+                val navBackStackEntry by navController.currentBackStackEntryAsState()
+                val bottomBarState = rememberSaveable { (mutableStateOf(true)) }
+                
+                Scaffold(bottomBar = { BottomNavView(navController = navController, bottomBarState = bottomBarState) }) { innerPadding ->
+                    var paddingValue = innerPadding
+                    when (navBackStackEntry?.destination?.route) {
+                        "movie_list_screen" -> {
+                            bottomBarState.value = true
+                        }
+                        "favorite_list_screen" -> {
+                            bottomBarState.value = true
+                        }
+                        else -> {
+                            bottomBarState.value = false
+                            paddingValue = PaddingValues(0.dp)
+                        }
+                    }
+                    NavGraphView(navController = navController, sharedViewModel = sharedViewModel, modifier = Modifier.padding(paddingValues = paddingValue))
                 }
             }
         }
--- app/src/main/java/levilin/moviedatabase/ui/navigation/BottomNavView.kt
+++ app/src/main/java/levilin/moviedatabase/ui/navigation/BottomNavView.kt
@@ -1,7 +1,11 @@
 package levilin.moviedatabase.ui.navigation
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -12,37 +16,46 @@ import levilin.moviedatabase.ui.theme.buttonIconColor
 
 
 @Composable
-fun BottomNavView(navController: NavController) {
+fun BottomNavView(navController: NavController, bottomBarState: MutableState<Boolean>) {
     val items = listOf(
         BottomNavItem.MovieList,
         BottomNavItem.FavoriteList
     )
-    BottomNavigation(
-        backgroundColor = MaterialTheme.colors.buttonBackgroundColor,
-        contentColor = MaterialTheme.colors.buttonIconColor
-    ) {
-        val navBackStackEntry by navController.currentBackStackEntryAsState()
-        val currentRoute = navBackStackEntry?.destination?.route
-        items.forEach { item ->
-            BottomNavigationItem(
-                icon = { Icon(imageVector = item.icon, contentDescription = item.title) },
-                label = { Text(text = item.title, fontSize = 9.sp) },
-                selectedContentColor = MaterialTheme.colors.buttonIconColor,
-                unselectedContentColor = MaterialTheme.colors.buttonIconColor.copy(0.4f),
-                alwaysShowLabel = true,
-                selected = currentRoute == item.route,
-                onClick = {
-                    navController.navigate(item.route) {
-                        navController.graph.startDestinationRoute?.let { screen_route ->
-                            popUpTo(screen_route) {
-                                saveState = true
+
+    AnimatedVisibility(
+        visible = bottomBarState.value,
+        enter = slideInVertically(initialOffsetY = { it }),
+        exit = slideOutVertically(targetOffsetY = { it }),
+        content = {
+            BottomNavigation(
+                backgroundColor = MaterialTheme.colors.buttonBackgroundColor,
+                contentColor = MaterialTheme.colors.buttonIconColor
+            ) {
+                val navBackStackEntry by navController.currentBackStackEntryAsState()
+                val currentRoute = navBackStackEntry?.destination?.route
+
+                items.forEach { item ->
+                    BottomNavigationItem(
+                        icon = { Icon(imageVector = item.icon, contentDescription = item.title) },
+                        label = { Text(text = item.title, fontSize = 9.sp) },
+                        selectedContentColor = MaterialTheme.colors.buttonIconColor,
+                        unselectedContentColor = MaterialTheme.colors.buttonIconColor.copy(0.4f),
+                        alwaysShowLabel = true,
+                        selected = currentRoute == item.route,
+                        onClick = {
+                            navController.navigate(item.route) {
+                                navController.graph.startDestinationRoute?.let { screen_route ->
+                                    popUpTo(screen_route) {
+                                        saveState = true
+                                    }
+                                }
+                                launchSingleTop = true
+                                restoreState = true
                             }
                         }
-                        launchSingleTop = true
-                        restoreState = true
-                    }
+                    )
                 }
-            )
+            }
         }
-    }
+    )
 }
